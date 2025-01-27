770d462d5b51f7598413f82ece5f4935a930af4c
Rodolfo <rqm@outlook.es>
2025-01-26 20:31:16 -0600

    uno

---
+++ event/Program.cs
@@ -0,0 +1,46 @@
+﻿
+public delegate void AlertaDeCalorHandler();
+class Termometro
+{
+    public event AlertaDeCalorHandler AlertaDeCalor;
+
+    public void MedirTemperatura()
+    {
+        for (int i = 0; i < 35; i++)
+        {
+            if (i < 29)
+            {
+                Console.WriteLine($"La temperatura es adecuada {i}°");
+            }
+            else
+            {
+                Console.WriteLine($"La temperatura es alta {i}°");
+                AlertaDeCalor?.Invoke();
+            }
+        }
+    }
+}
+
+
+class Program
+{
+    static void Main()
+    {
+        Termometro termo = new Termometro();
+
+        termo.AlertaDeCalor += MensajeIncio;
+        termo.AlertaDeCalor += MensajeAdvertencia;
+
+        termo.MedirTemperatura();
+    }
+
+    public static void MensajeIncio()
+    {
+        Console.WriteLine("Bienvenido a la aplicación de termometro");
+    }
+
+    public static void MensajeAdvertencia()
+    {
+        Console.WriteLine("Cuidado temperatura muy alta");
+    }
+}
