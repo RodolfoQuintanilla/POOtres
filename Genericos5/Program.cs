e2e20b0963f1852a87ea35abbd6fb93b5d7a5c6c
Rodolfo <rqm@outlook.es>
2025-02-04 23:24:55 -0600

    uno

---
+++ Genericos5/Program.cs
@@ -0,0 +1,31 @@
+﻿public class Calculadora
+{
+    public static int Sumar(int a, int b)
+    {
+        return a + b;
+    }
+
+    public static int Restar(int a, int b) => a - b;
+    public static int Multiplicar(int a, int b) => a * b;
+
+    public static int Dividir(int a, int b){
+        if (b == 0) {
+            throw new DivideByZeroException("No se puede dividir por cero");
+        }else
+        {
+            return a / b;
+        }
+    }
+
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Calculadora.Sumar(5, 5));
+        Console.WriteLine(Calculadora.Restar(5, 9));
+        Console.WriteLine(Calculadora.Multiplicar(5, 9));
+        Console.WriteLine(Calculadora.Dividir(5, 9));
+    }
+}
